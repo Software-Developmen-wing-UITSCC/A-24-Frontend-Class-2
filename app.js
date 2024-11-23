0e0930b7871f3ef222f77a9e92891e46f94c9024
AkibReza <akib.reza23@gmail.com>
2024-11-23 22:59:06 +0600

    class 2

---
+++ app.js
@@ -0,0 +1,64 @@
+// Voter;
+
+//var age = prompt("Enter your age");
+/* if (age >= 18) {
+  console.log("You can vote!");
+} else {
+  console.log("You cannot vote!");
+} */
+
+/* ------------------------------------------- */
+
+// Odd display
+
+/* for (var i = 1; i < 100; i += 2) {
+  console.log(i);
+} */
+
+/* ------------------------------------------- */
+// Access arrays
+
+/* var students = ["Rayhan", "Joy", "Nazib", "Arbin"];
+
+console.log(students.length); // 4 */
+
+// 0 1 2 3
+
+/* for (var i = 0; i < 4; i++) {
+  if (i == 1) {
+    break;
+  } else {
+    console.log("Hello: ", students[i]);
+  }
+} */
+
+/* ------------------------------------------- */
+
+//String and Integer
+
+/* var name = "Akib";
+var name2 = "Reza";
+
+console.log(name + " " + name2);
+
+console.log(1 + "1");
+
+ */
+
+//Write file names like this:
+//camelCase
+//or_eivabe_use_korte_paro
+
+//console.log("Hello world!!");
+
+//HW:
+
+/* 
+1. Take a number with prompt. Print if its even or odd.
+
+2. Take a number with prompt. Using loop display numbers that are divisible by 2 and 5 numbers until that range.
+
+Example: 10 20 30 etc..
+
+3. Write an array with 15 values. Display every 3rd value only.   
+*/
---
+++ project_2/newApp.js
@@ -0,0 +1,17 @@
+var students = ["Rayhan", "Joy", "Nazib", "Arbin"];
+
+var newFunction = (name) => {
+  console.log("Hello: " + name);
+};
+
+for (var i = 0; i < students.length; i++) {
+  newFunction(students[i]);
+}
+
+// students[i] => students[0] => "Rayhan" => newFunction("Rayhan")
+
+// Variable
+// Loop
+// Conditions
+// Array
+// Function
